4a3d941e39eb2bdb22459915fb3d870e76e5e23f
Ivan Smith <ivasmi4862@students.ecpi.edu>
2020-08-18 21:48:06 -0400

    Updates

---
+++ precache-manifest.69576259c9abdd99a94f6e4cbea6f94c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b76dbeedc31d80fd5c24d59a1456d9dd",
+    "url": "/course-project/index.html"
+  },
+  {
+    "revision": "46640d2b3c896ebb08b2",
+    "url": "/course-project/static/css/main.d802c456.chunk.css"
+  },
+  {
+    "revision": "d46b519f75669f3f9761",
+    "url": "/course-project/static/js/2.6394cd40.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/course-project/static/js/2.6394cd40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46640d2b3c896ebb08b2",
+    "url": "/course-project/static/js/main.3a8f8b9d.chunk.js"
+  },
+  {
+    "revision": "19e214ab76609767057a",
+    "url": "/course-project/static/js/runtime-main.9276f70a.js"
+  }
+]);
